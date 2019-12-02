b7758b8065e139ddf47eb4fa24eede39edc178c9
Walid el Harrak <harrak31@gmail.com>
2019-12-02 09:46:10 +0100

    samba:base

--- samba19:base/startup.sh
+++ samba19:base/startup.sh
@@ -2,9 +2,19 @@
 # @edt ASIX M06 2019-2020
 # startup.sh
 # -------------------------------------
-
+#install.sh
 /opt/docker/install.sh && echo "Install Ok"
-/usr/sbin/smbd && echo "smb Ok"
-/usr/sbin/nmbd && echo "nmb  Ok"
-/bin/bash
 
+#creacio users
+useradd lila
+useradd roc
+useradd patipla
+useradd pla
+echo -e "lila\nlila" | smbpasswd -a lila
+echo -e "roc\nroc" | smbpasswd -a roc
+echo -e "patipla\npatipla" | smbpasswd -a patipla
+echo -e "pla\npla" | smbpasswd -a pla
+
+#iniciar serveis
+/usr/sbin/smbd 
+/usr/sbin/nmbd -F
